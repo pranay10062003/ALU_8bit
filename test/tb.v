5638ce7f32911c6a9079cabce9bfc99fa7b1c078
pranay10062003 <pv2577@srmist.edu.in>
2025-09-09 11:51:54 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,32 +1,59 @@
+`timescale 1ns/1ps
 `default_nettype none
-`timescale 1ns / 1ps
 
-/* This testbench just instantiates the module and makes some convenient wires
-   that can be driven / tested by the cocotb test.py.
-*/
-module ALU_4bit_tb;
+module tb_tt_um_example;
+
+  reg  [7:0] ui_in;
+  reg  [7:0] uio_in;
   wire [7:0] uo_out;
-  reg [3:0] ena;
-  reg [3:0] ui_in,uio_in;
-  tt_um_ALU4bit sai (.ui_in(ui_in),.uio_in(uio_in),.ena(ena),.uo_out(uo_out));
-  initial
-    begin
-      $dumpfile("dump.vcd"); $dumpvars;
-      ui_in = 4'b1110;
-      uio_in = 4'b1001;
-      ena = 4'b0000; #20;
-      ena = 4'b0001; #20;
-      ena = 4'b0010; #20;
-      ena = 4'b0011; #20;
-      ena = 4'b0100; #20;
-      ena = 4'b0101; #20;
-      ena = 4'b0110; #20;
-      ena = 4'b0111; #20;
-      ena = 4'b1000; #20;
-      ena = 4'b1001; #20;
-      ena = 4'b1010; #20;
-      ena = 4'b1011; #20;
-      ena = 4'b1100; #20;
-      $finish;
-    end
+  wire [7:0] uio_out;
+  wire [7:0] uio_oe;
+  reg        ena;
+  reg        clk;
+  reg        rst_n;
+
+  // DUT
+  tt_um_example dut (
+    .ui_in(ui_in),
+    .uo_out(uo_out),
+    .uio_in(uio_in),
+    .uio_out(uio_out),
+    .uio_oe(uio_oe),
+    .ena(ena),
+    .clk(clk),
+    .rst_n(rst_n)
+  );
+
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars;
+
+    ena   = 1;
+    clk   = 0;
+    rst_n = 1;
+
+    ui_in  = 8'b11101111; // operand1 (239), sel initially 1110
+    uio_in = 8'b10011110; // operand2 (158)
+
+    // Apply different opcodes
+    ui_in[7:4] = 4'b0000; #20; // ADD
+    ui_in[7:4] = 4'b0001; #20; // SUB
+    ui_in[7:4] = 4'b0010; #20; // MUL
+    ui_in[7:4] = 4'b0011; #20; // DIV
+    ui_in[7:4] = 4'b0100; #20; // AND
+    ui_in[7:4] = 4'b0101; #20; // OR
+    ui_in[7:4] = 4'b0110; #20; // NOT in1
+    ui_in[7:4] = 4'b0111; #20; // NOT in2
+    ui_in[7:4] = 4'b1000; #20; // SQUARE in1
+    ui_in[7:4] = 4'b1001; #20; // SQUARE in2
+    ui_in[7:4] = 4'b1010; #20; // LESS THAN
+    ui_in[7:4] = 4'b1011; #20; // EQUAL
+    ui_in[7:4] = 4'b1100; #20; // GREATER THAN
+
+    $finish;
+  end
+
+  // Optional clock (not used by ALU, but kept for skeleton)
+  always #5 clk = ~clk;
+
 endmodule
