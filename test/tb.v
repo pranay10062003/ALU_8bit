f142f8324bebdd9736a0659ad83aaf989768f762
pranay10062003 <pv2577@srmist.edu.in>
2025-09-09 11:21:44 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -4,46 +4,29 @@
 /* This testbench just instantiates the module and makes some convenient wires
    that can be driven / tested by the cocotb test.py.
 */
-module tb ();
-
-  // Dump the signals to a VCD file. You can view it with gtkwave or surfer.
-  initial begin
-    $dumpfile("tb.vcd");
-    $dumpvars(0, tb);
-    #1;
-  end
-
-  // Wire up the inputs and outputs:
-  reg clk;
-  reg rst_n;
-  reg ena;
-  reg [7:0] ui_in;
-  reg [7:0] uio_in;
+module ALU_4bit_tb;
   wire [7:0] uo_out;
-  wire [7:0] uio_out;
-  wire [7:0] uio_oe;
-`ifdef GL_TEST
-  wire VPWR = 1'b1;
-  wire VGND = 1'b0;
-`endif
-
-  // Replace tt_um_example with your module name:
-  tt_um_example user_project (
-
-      // Include power ports for the Gate Level test:
-`ifdef GL_TEST
-      .VPWR(VPWR),
-      .VGND(VGND),
-`endif
-
-      .ui_in  (ui_in),    // Dedicated inputs
-      .uo_out (uo_out),   // Dedicated outputs
-      .uio_in (uio_in),   // IOs: Input path
-      .uio_out(uio_out),  // IOs: Output path
-      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
-      .ena    (ena),      // enable - goes high when design is selected
-      .clk    (clk),      // clock
-      .rst_n  (rst_n)     // not reset
-  );
-
+  reg [3:0] ena;
+  reg [3:0] ui_in,uio_in;
+  ALU_4bit sai (.ui_in(ui_in),.uio_in(uio_in),.ena(ena),.uo_out(uo_out));
+  initial
+    begin
+      $dumpfile("dump.vcd"); $dumpvars;
+      ui_in = 4'b1110;
+      uio_in = 4'b1001;
+      ena = 4'b0000; #20;
+      ena = 4'b0001; #20;
+      ena = 4'b0010; #20;
+      ena = 4'b0011; #20;
+      ena = 4'b0100; #20;
+      ena = 4'b0101; #20;
+      ena = 4'b0110; #20;
+      ena = 4'b0111; #20;
+      ena = 4'b1000; #20;
+      ena = 4'b1001; #20;
+      ena = 4'b1010; #20;
+      ena = 4'b1011; #20;
+      ena = 4'b1100; #20;
+      $finish;
+    end
 endmodule
